414144c87b718ba4229b8203bcc60bb0fb919bfe
Chaitrali R <chaitrali_r@tekditechnologies.com>
2023-03-28 20:33:35 +0530

    Show record names

--- src/app/issure/bulk-records/bulk-records.component.ts
+++ src/app/issure/bulk-records/bulk-records.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { GeneralService } from 'src/app/services/general/general.service';
+import { AppConfig } from 'src/app/app.config';
 
 @Component({
   selector: 'app-bulk-records',
@@ -7,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulkRecordsComponent implements OnInit {
   headerName: string = 'plain';
-  constructor() { }
-
+  pdfName: any;
+  osid: string;
+  schemaObj;
+  tempObj: any;
+  nameArray = [];
+  nameArray2;
+  constructor(public router: Router, public route: ActivatedRoute,
+    public generalService: GeneralService, private http: HttpClient,
+    private config: AppConfig){
+      this.osid = this.route.snapshot.paramMap.get('osid'); 
+    }
   ngOnInit(): void {
+   
+   
 
+    console.log(this.osid);
+
+    
+    this.generalService.getData('/Schema/' + this.osid).subscribe((res) => {
+     console.log(res);
+     this.schemaObj = JSON.parse(res.schema);
+     this.tempObj = this.schemaObj.definitions.ScholarshipForTopClassStudents.properties;
+       console.log(this.tempObj);
+       Object.values(this.tempObj).forEach(entry => {
+         console.log(entry['title']);
+         this.nameArray.push(entry['title']); 
+       });
+     
+     console.log(this.nameArray);
+     this.nameArray2 = this.nameArray.join();
+     console.log(this.nameArray2);
+      // this.downloadCSV(this.nameArray2);
+     }, err=>{
+       
+       console.log(err);
+     });
+    
   }
 
 }
