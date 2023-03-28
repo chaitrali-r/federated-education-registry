64fea8754cb6112411ae767749bf05efa5d08de8
suvarnakale <suvarna.kale@tekditechnologies.com>
2023-03-28 17:11:20 +0530

    Bulk record import UI done

--- src/app/issure/bulk-records/bulk-records.component.ts
+++ src/app/issure/bulk-records/bulk-records.component.ts
@@ -10,17 +10,7 @@ export class BulkRecordsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
-  }
 
-  updateLabel(input) {
-    var label = document.getElementById("file-label");
-    if (input.files.length > 0) {
-      label.textContent = input.files[0].name;
-    } else {
-      label.textContent = "Browse File";
-    }
   }
-  
 
 }
